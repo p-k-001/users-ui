0e606868d563823fbb25175eb7f7c652ffe88e73
pk <pavel_kohout@centrum.cz>
2025-07-22 14:59:29 +0200

    remove login from swagger page

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Swagger from "./pages/Swagger";
-import TestApiForm from "./pages/TestApiForm";
-
-import { useEffect, useState } from "react";
-import LoginHeader from "./components/LoginHeader";
-
-interface ErrorMessage {
-  msg?: string;
-  message?: string;
-}
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
-    !!localStorage.getItem("token")
-  );
-  const [email, setEmail] = useState<string>("");
-  const [message, setMessage] = useState<string | null>(null);
-  const [error, setError] = useState<ErrorMessage | ErrorMessage[] | null>(
-    null
-  );
-
-  useEffect(() => {
-    const storedEmail = localStorage.getItem("userEmail");
-    setEmail(storedEmail || "");
-  }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setIsLoggedIn(false);
-  };
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    const storedEmail = localStorage.getItem("userEmail");
-    setEmail(storedEmail || "");
-  };
   return (
     <div>
       <BrowserRouter>
-        <LoginHeader
-          isLoggedIn={isLoggedIn}
-          email={email}
-          handleLogout={handleLogout}
-          handleLogin={handleLogin}
-          clearMessage={() => setMessage(null)}
-          clearErrorMessage={() => setError(null)}
-        />
-
         <Routes>
-          <Route
-            index
-            element={
-              <TestApiForm
-                isLoggedIn={isLoggedIn}
-                message={message}
-                setMessage={setMessage}
-                error={error}
-                setError={setError}
-              />
-            }
-          />
+          <Route index element={<HomePage />} />
           <Route path="swagger" element={<Swagger />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,56 @@
+import TestApiForm from "../components/TestApiForm";
+import LoginHeader from "../components/LoginHeader";
+
+import { useEffect, useState } from "react";
+
+interface ErrorMessage {
+  msg?: string;
+  message?: string;
+}
+
+export default function HomePage() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
+    !!localStorage.getItem("token")
+  );
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string | null>(null);
+  const [error, setError] = useState<ErrorMessage | ErrorMessage[] | null>(
+    null
+  );
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("userEmail");
+    setEmail(storedEmail || "");
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    const storedEmail = localStorage.getItem("userEmail");
+    setEmail(storedEmail || "");
+  };
+  return (
+    <div>
+      <LoginHeader
+        isLoggedIn={isLoggedIn}
+        email={email}
+        handleLogout={handleLogout}
+        handleLogin={handleLogin}
+        clearMessage={() => setMessage(null)}
+        clearErrorMessage={() => setError(null)}
+      />
+
+      <TestApiForm
+        isLoggedIn={isLoggedIn}
+        message={message}
+        setMessage={setMessage}
+        error={error}
+        setError={setError}
+      />
+    </div>
+  );
+}
